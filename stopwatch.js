228716db2e3ee66b3cac7dd12f02c1d64014add7
NishantChandel7 <153085923+NishantChandel7@users.noreply.github.com>
2023-12-06 17:32:08 +0530

    Add files via upload

---
+++ stopwatch.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let stopwatchInterval;
+    let isRunning = false;
+    let seconds = 0;
+    let minutes = 0;
+    let hours = 0;
+
+    function updateDisplay() {
+      document.getElementById("stopwatchDisplay").textContent = formatTime();
+    }
+
+    function formatTime() {
+      const formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+      const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+      const formattedHours = hours < 10 ? "0" + hours : hours;
+
+      return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    }
+
+    function startStopwatch() {
+      stopwatchInterval = setInterval(function() {
+        seconds++;
+        if (seconds === 60) {
+          seconds = 0;
+          minutes++;
+        }
+        if (minutes === 60) {
+          minutes = 0;
+          hours++;
+        }
+        updateDisplay();
+      }, 1000);
+    }
+
+    function stopStopwatch() {
+      clearInterval(stopwatchInterval);
+    }
+
+    function toggleStopwatch() {
+        const startStopButton = document.getElementById("startStopButton");
+      
+        if (isRunning) {
+          stopStopwatch();
+          startStopButton.textContent = "Start";
+          startStopButton.classList.remove("btn-danger");
+          startStopButton.classList.add("btn-primary");
+        } else {
+          startStopwatch();
+          startStopButton.textContent = "Stop";
+          startStopButton.classList.remove("btn-primary");
+          startStopButton.classList.add("btn-danger");
+        }
+      
+        isRunning = !isRunning;
+      }
+      
+    document.getElementById("startStopButton").addEventListener("click", toggleStopwatch);
+  });
