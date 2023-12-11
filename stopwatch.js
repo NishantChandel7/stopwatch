18940b4e6dbb9f41b4eacf91e22c14a584fe9984
NishantChandel7 <153085923+NishantChandel7@users.noreply.github.com>
2023-12-11 18:19:16 +0530

    Update stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -1,58 +1,80 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let stopwatchInterval;
-    let isRunning = false;
-    let seconds = 0;
-    let minutes = 0;
-    let hours = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  let stopwatchInterval;
+  let isRunning = false;
+  let seconds = 0;
+  let minutes = 0;
+  let hours = 0;
 
-    function updateDisplay() {
+  function updateDisplay() {
       document.getElementById("stopwatchDisplay").textContent = formatTime();
-    }
+  }
 
-    function formatTime() {
+  function formatTime() {
       const formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
       const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
       const formattedHours = hours < 10 ? "0" + hours : hours;
 
       return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-    }
+  }
 
-    function startStopwatch() {
-      stopwatchInterval = setInterval(function() {
-        seconds++;
-        if (seconds === 60) {
-          seconds = 0;
-          minutes++;
-        }
-        if (minutes === 60) {
-          minutes = 0;
-          hours++;
-        }
-        updateDisplay();
+  function startStopwatch() {
+      stopwatchInterval = setInterval(function () {
+          seconds++;
+          if (seconds === 60) {
+              seconds = 0;
+              minutes++;
+          }
+          if (minutes === 60) {
+              minutes = 0;
+              hours++;
+          }
+          updateDisplay();
       }, 1000);
-    }
+  }
 
-    function stopStopwatch() {
+  function stopStopwatch() {
       clearInterval(stopwatchInterval);
-    }
+  }
 
-    function toggleStopwatch() {
-        const startStopButton = document.getElementById("startStopButton");
-      
-        if (isRunning) {
+  function resetFunction() {
+      stopStopwatch();
+      seconds = 0;
+      minutes = 0;
+      hours = 0;
+      updateDisplay();
+      isRunning = false;
+
+      const startStopButton = document.getElementById("startStopButton");
+      startStopButton.textContent = "Start";
+      startStopButton.classList.remove("btn-danger");
+      startStopButton.classList.add("btn-primary");
+
+      console.log('Timer reset!');
+  }
+
+  function toggleStopwatch() {
+      const startStopButton = document.getElementById("startStopButton");
+      var resetButton = document.getElementById('rstBtn');
+
+      resetButton.addEventListener('click', function () {
+          resetFunction();
+      });
+
+      if (isRunning) {
           stopStopwatch();
           startStopButton.textContent = "Start";
           startStopButton.classList.remove("btn-danger");
           startStopButton.classList.add("btn-primary");
-        } else {
+
+      } else {
           startStopwatch();
           startStopButton.textContent = "Stop";
           startStopButton.classList.remove("btn-primary");
           startStopButton.classList.add("btn-danger");
-        }
-      
-        isRunning = !isRunning;
       }
-      
-    document.getElementById("startStopButton").addEventListener("click", toggleStopwatch);
-  });
+
+      isRunning = !isRunning;
+  }
+
+  document.getElementById("startStopButton").addEventListener("click", toggleStopwatch);
+});
